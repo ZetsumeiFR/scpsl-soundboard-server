ac87aacf83228bdfd25d0609f90a86010288275e
Lou Tigroudja <lou.tigroudja07@gmail.com>
2026-01-30 23:33:44 +0100

    (fix): Fixed CORS policy

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,6 +4,6 @@ import { env } from "../config/env.js";
 export const corsMiddleware = cors({
   origin: env.frontendUrl,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 });
